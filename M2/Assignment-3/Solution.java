7d8331df8c3841cac354c1890096dee6773e73c4
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-28 18:02:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- M2/Assignment-3/Solution.java
+++ M2/Assignment-3/Solution.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+/**SQUARE OF SQUARES.**/
+final class Solution {
+    /**
+    * Do not modify this main function.
+    */
+    private Solution() { }
+    /**
+     * @param args comment
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result=power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
-	}
-	public static int power(int base,int exponent) {
-		if(exponent==0) {
-			return 1;
-		}
-		else if(exponent==1){
-			return base;
-		}
-		else{
-			return base * power(base,exponent-1);
-		}
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /**
+    * @param base a
+    * @param exponent b
+    **/
+    public static long power(final int base, final int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        else if (exponent == 1) {
+            return base;
+        }
+        else {
+            return base * power(base, exponent - 1);
+        }
+    }
 }
+
