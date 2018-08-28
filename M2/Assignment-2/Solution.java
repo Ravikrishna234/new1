cc141f4af58802f9e8c52b3ad56064627a0d5f45
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-28 17:54:16 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- M2/Assignment-2/Solution.java
+++ M2/Assignment-2/Solution.java
@@ -1,24 +1,34 @@
-import java.util.Scanner;
-import java.lang.*;
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
+import java.util.Scanner;
+/**This is a program.**/
+
+final class Solution {
+	/**
+	*Do not modify this main function.
+	**/
+	private Solution() { }
+	/**
+	* @param args comment
+	**/ 
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
+		rootsEquation(a, b, c);
 	}
-	public static void rootsOfQuadraticEquation(int a,int b,int c) {
-		double d = (b*b)-(4*a*c);
-		double result = -b+(Math.sqrt(d));
-		result = result / (2*a);
-		double result1 = -b-(Math.sqrt(d));
-		result1 = result1 / (2*a);
-		System.out.println(result+" "+result1);
+	/**
+	* @param a a
+	* @param b b
+	* @param c c
+	**/
+	public static void rootsEquation(final int a, final int b, final int c) {
+		double d = (b * b) - (4 * a * c);
+		double result = - b + (Math.sqrt(d));
+		result = result / (2 * a);
+		double result1 = -b - (Math.sqrt(d));
+		result1 = result1 / (2 * a);
+		System.out.println(result + " " + result1);
 
 	}
 	/*
