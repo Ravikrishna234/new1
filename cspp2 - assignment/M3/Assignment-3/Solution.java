b2c51114c37878fade6ff081da57a8660cb4ad66
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-29 17:27:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 - assignment/M3/Assignment-3/Solution.java
+++ cspp2 - assignment/M3/Assignment-3/Solution.java
@@ -1,51 +1,34 @@
-
 import java.util.Scanner;
-/*
-    Do not modify this main function.
-    */
+/**Do not modify this main function.**/
 final class Solution {
     /**
      * @brief [brief description]
      * @details [long description]
-     * 
-     * @param args [description]
      */
-    private Solution(){}
+    private Solution() { }
     /**
-     * @brief [brief description]
-     * @details [long description]
-     * 
      * @param args [description]
      */
-
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         int result;
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        result = gcd(n1,n2);
+        result = gcd(n1, n2);
         System.out.println(result);
     }
     /**
-     * @brief [brief description]
-     * @details [long description]
-     * 
-     * @param int [description]
-     * @param int [description]
-     * 
-     * @return [description]
-     */
-    public static int gcd(final int a,final int b) {
-        int remainder=0;
-        
-       for(int i=1;i<=a && i<=b;i++)
-       {
-        if(a%i ==0 && b%i ==0) {
+     * @param a variable1
+     * @param b variable2
+     * @return remainder
+     **/
+    public static int gcd(final int a, final int b) {
+        int remainder = 0, i;
+        for (i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0) {
             remainder = i;
         }
-
-       }
+    }
        return remainder;
-        
 }
 }
