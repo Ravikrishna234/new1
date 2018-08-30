fa4b847a8a04dea812ee3734773b3c26d16fb0f9
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-30 14:31:53 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2 - assignment/M4/Assignment-2/Solution.java
+++ cspp2 - assignment/M4/Assignment-2/Solution.java
@@ -1,32 +1,30 @@
 import java.util.*;
 public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	 {
 		Scanner s = new Scanner(System.in);
-		
 		int row1 = s.nextInt();
 		int col1 = s.nextInt();
 		int a[][] = new int[row1][col1];
-		
-		for(int i=0;i<row1;i++){
-			for(int j=0;j<col1;j++){
-				a[i][j] = s.nextInt();
-			}
-		}
 		int row2 = s.nextInt();
 		int col2 = s.nextInt();
 		int b[][] = new int[row2][col2];
-
+		if(col1 == row1 && row2 == col2)
+		{
+		for(int i=0; i<row1;i++)
+		{
+			for(int j=0;j<col1;j++)
+			{
+				a[i][j] = s.nextInt();
+			}
+		}
 		for(int i=0;i<row2;i++)
 		{
 			for(int j=0;j<col2;j++)
 			{
 				b[i][j] = s.nextInt();
 			}
-		}
-		if(col1!=row1 || col2!=row2){
-			System.out.println("not possible");
-
 		}
 		int result[][] = new int[row1][col2];
 		for(int i=0;i<row1;i++)
@@ -37,18 +35,20 @@ public class Solution {
 
 			}
 		}
-		for(int i=0;i<row1;i++){
-			for(int j=0;j<col2;j++){
+		for(int i=0;i<row1;i++)
+		{
+			for(int j=0;j<col2;j++)
+			{
 
-			if(j<col2-1){
-		
+		if(j<col2-1)
 		System.out.print(result[i][j]+" ");
-	}
-	else{
+		else
 		System.out.print(result[i][j]);
-	}
-	}
+	        }
 	System.out.print("\n");
+        }
 }
+else
+System.out.println("not possible");
 }
 }
