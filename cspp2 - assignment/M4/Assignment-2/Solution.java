ad217600e1a383ddb401696e2ffa9648f3e5797c
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-30 14:15:39 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- cspp2 - assignment/M4/Assignment-2/Solution.java
+++ cspp2 - assignment/M4/Assignment-2/Solution.java
@@ -3,21 +3,20 @@ public class Solution {
 	/* Fill the main function to print resultant of addition of matrices*/
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter row and column");
+		
 		int row1 = s.nextInt();
 		int col1 = s.nextInt();
 		int a[][] = new int[row1][col1];
-		System.out.println("Enter matrix 1");
+		
 		for(int i=0;i<row1;i++){
 			for(int j=0;j<col1;j++){
 				a[i][j] = s.nextInt();
 			}
 		}
-		System.out.println("Enter row and column");
 		int row2 = s.nextInt();
 		int col2 = s.nextInt();
 		int b[][] = new int[row2][col2];
-		System.out.println("Enter matrix 2");
+
 		for(int i=0;i<row2;i++)
 		{
 			for(int j=0;j<col2;j++)
