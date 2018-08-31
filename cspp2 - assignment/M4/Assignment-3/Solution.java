30800661814e9e759454fdcb6abd6819f8ea0696
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-31 15:33:23 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2 - assignment/M4/Assignment-3/Solution.java
+++ cspp2 - assignment/M4/Assignment-3/Solution.java
@@ -1,29 +1,34 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
+/**BINARYTODECIMAL.**/
 public class Solution
 {
 	/*
 	Do not modify this main function.
 	*/
+	private Solution() { }
+	/**
+	 * @param args [description]
+	 */
 	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		String res="";
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
-			res=binaryToDecimal(s);//Write binaryToDecimal function	
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		String res = "";
+		for(int i = 0; i < n; i++){
+			String s = sc.nextLine();
+			res = binaryToDecimal(s);//Write binaryToDecimal function	
 			System.out.println(res);
 		}
 	}
-	final static String binaryToDecimal(String str) 
+	/**
+	 * @param str String
+	 * @return value value
+	 */
+	public static String binaryToDecimal(String str) 
 	{
 		int decimal=0;
 		int base = 1;
-		for(int i=str.length()-1;i>=0;i--)
+		for(int i = str.length() - 1; i >= 0; i--)
 		{
 			char ch = str.charAt(i);
 			int num = Integer.parseInt(str.valueOf(ch));
