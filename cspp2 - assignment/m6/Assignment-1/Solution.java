28da5f6106965c163bbc93581130920270e0f6e9
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-01 10:10:06 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- cspp2 - assignment/m6/Assignment-1/Solution.java
+++ cspp2 - assignment/m6/Assignment-1/Solution.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
-
+/**COMPOSITENUMBERS.**/
 public final class Solution {
     /**
     * Empty constructor.
     */
     private Solution() { }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @param n value
+     */
     public static void oddComposites(final int n) {
-    	int j;
-    	for(int i=2;i<=n;i++)
-    	{
-    		int count = 0;
-    		for(j=i;j>=1;j--)
-    		{
-    			if(i%j == 0)
-    			{
-    				count++;
-    			}
-    		}
-    		if(count>2)
-    			{
-    				if(i%2!=0)
-    				{
-    					System.out.println(i);
-    				}
-    			}
-    		}
-    	}
-    
+        int j;
+        for (int i = 2; i <= n; i++) {
+            int count = 0;
+            for (j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count > 2) {
+                    if (i % 2 != 0) {
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+        /**
+         * @param args value
+         */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
