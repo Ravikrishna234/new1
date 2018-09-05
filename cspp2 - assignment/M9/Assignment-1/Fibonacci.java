f04a62715bf3395140a703f22a223d54a2bebd7e
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-05 10:31:27 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 - assignment/M9/Assignment-1/Fibonacci.java
+++ cspp2 - assignment/M9/Assignment-1/Fibonacci.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
+/**FIBONACCI.**/
 /*
  * In the previous module you have created List ADT.
  * ADTs are developed as reusable libraries
@@ -8,42 +7,43 @@ import java.util.Arrays;
  * A program that uses the ADT is called a "client" of the ADT
  * The client typically instantiates an object of the ADT
  * and invokes the public methods on the ADT object.
- *
  * This assignment is to practice the idea of client to the ADT.
  * Client class is Fibonacci and the client needs to use the List ADT
  * to store fibonacci numbers in the List ADT.
  * So, Fibonacci is going to instantiate an object of List ADT
  * generate fibonacci numbers and add them to the list
  * by invoking the add method provided in the List ADT.
- *
  */
-
-public class Fibonacci
-{
-    /*
-     * fib is a static method takes a argument n
-     * n is the count of the fibonacci numbers to be generated.
-     * The method returns a List with the n fibonacci numbers.
+final class Fibonacci {
+    /**
+    * @brief [brief description]
+    * @details [long description]
+    */
+    private Fibonacci() { }
+    /**
+     * @param n [description]
+     * @return value
      */
-    public static List fib(int n) {
-    	List l = new List();
-    	int a = 0;
-    	int b = 1;
-    	l.add(a);
-    	l.add(b);
-    	for(int i = 2; i < n; i++)
-    	{
-    		int c = a + b;
-    		l.add(c);
-    		a = b;
-    		b = c;
-    	}
-    	return l;
+    public static List fib(final int n) {
+        List l = new List(n);
+        int a = 0;
+        int b = 1;
+        l.add(a);
+        l.add(b);
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            l.add(c);
+            a = b;
+            b = c;
+        }
+        return l;
     }
-
-	public static void main(String[] args) {
+    /**
+     * @param args value
+     */
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-       	System.out.println(fib(n));
+        System.out.println(fib(n));
     }
 }
--- cspp2 - assignment/M9/Assignment-1/List.java
+++ cspp2 - assignment/M9/Assignment-1/List.java
@@ -60,9 +60,9 @@ public class List {
      * The purpose of the constructor is to initialize the
      * class variables with some default values.
      */
-    
-    
-
+    public List(int n) {
+        list = new int[n];
+    }
     public List() {
 
         // what are the two variables to be initialized here?
@@ -70,7 +70,7 @@ public class List {
         // What should be the default values?
         // In the case of the list, it should be empty but
         // it should be initialized with an array size like 10
-        list = new int[50];
+        list = new int[10];
 
         // Think about the initial value for size.
         // How many items do we have in the list when you create it?
