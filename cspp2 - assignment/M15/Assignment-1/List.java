046465e11e3197cf13e1de9cd0ca7e998b6780a5
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-11 15:02:27 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2 - assignment/M15/Assignment-1/List.java
+++ cspp2 - assignment/M15/Assignment-1/List.java
@@ -312,6 +312,7 @@ public class List {
             for (int i = 0; i < size; i++) {
                 if (list[i] == c) {
                     remove(i);
+                    i--;
                 }
             }
         }
