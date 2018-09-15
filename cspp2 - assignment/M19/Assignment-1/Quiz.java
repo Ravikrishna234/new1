8a2b93d0c48ec8370e7250756796a03b6784a211
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-15 11:53:31 +0530

    "check_style_score -> 1 of 13 passed. style score: 0/1 "

---
+++ cspp2 - assignment/M19/Assignment-1/Quiz.java
@@ -0,0 +1,19 @@
+class Quiz {
+    Quiz(){}
+    	String question;
+    	String choice;
+    	String answer;
+    	String Maximum_marks;
+    	String penalty;
+        String size;
+    	Quiz(String q,String c,String a,String max,String p) {
+    		this.question = q;
+    		this.choice = c;
+    		this.answer = a;
+    		this.Maximum_marks = max;
+    		this.penalty = p;
+    	}
+    	Quiz(String q) {
+    		this.question = q;
+    	}
+    }
