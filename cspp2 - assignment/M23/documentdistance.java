b4d4545337803623df7f84a807cf04cae0346624
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-21 12:25:45 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2 - assignment/M23/documentdistance.java
+++ cspp2 - assignment/M23/documentdistance.java
@@ -125,9 +125,9 @@ class documentdistance {
             String x = scan.nextLine();
             File inputfile = new File(x);
             File[] files = inputfile.listFiles();
-            System.out.print("           ");
+            System.out.print("\t\t");
             for(int i = 0; i < files.length; i++) {
-                System.out.print(files[i].getName() + "  ");
+                System.out.print(files[i].getName() + "\t");
             }
             System.out.println();
             for(int i = 0; i < files.length;i++) {
